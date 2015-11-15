af76f689eddee6075e86e2a0fb5db19982fec78e
mw10104587 <mw10104587@gmail.com>
2015-11-15 12:24:20 -0500

    gh-pages setup

---
+++ js/bingo-calendar.js
@@ -0,0 +1,140 @@
+
+$(document).ready(function(){
+
+	// var width = 960,
+	var width = 430,
+	    height = 170,
+	    cellSize = 20; // cell size
+
+	var percent = d3.format(".1%"),
+	    format = d3.time.format("%Y-%m-%d");
+
+	var color = d3.scale.quantize()
+	    // .domain([-.05, .05])
+	    .range(d3.range(4).map(function(d) { return "q" + d + "-4"; }));
+
+	var svg = d3.select("#calendar-chart")
+	  .append("svg")
+	 	.data([2015])
+		.attr("id", "calendar-svg")
+	    .attr("width", width)
+	    .attr("height", height)
+	    // .attr("class", "YlGn")
+	    .attr("class", "Greens")
+	  .append("g")
+	    .attr("transform", "translate(" + ((width - cellSize * 17) / 2) + "," + (height - cellSize * 7 - 1) + ")");
+
+	svg.append("text")
+	    .attr("transform", "translate(-6," + cellSize * 3.5 + ")rotate(-90)")
+	    .style("text-anchor", "middle")
+	    .text(function(d) { return d; });
+
+	var rect = svg.selectAll(".day")
+	    .data(function(d) { return d3.time.days(new Date(d, 0, 1), new Date(d , 4, 1)); })
+	  .enter().append("rect")
+	    .attr("class", "day")
+	    .attr("width", cellSize)
+	    .attr("height", cellSize)
+	    .attr("x", function(d) { return d3.time.weekOfYear(d) * cellSize; })
+	    .attr("y", function(d) { return d.getDay() * cellSize; })
+	    .datum(format);
+
+	rect.append("title")
+	    .text(function(d) { return d; });
+
+	svg.selectAll(".month")
+	    .data(function(d) { return d3.time.months(new Date(d, 0, 1), new Date(d, 4, 1)); })
+	  .enter().append("path")
+	    .attr("class", "month")
+	    .attr("d", monthPath);
+
+	d3.csv("data/bingo-date.csv", function(error, csv) {
+	  if (error) throw error;
+
+	  color.domain([3960, 11000]);
+
+	  var data = d3.nest()
+	    .key(function(d) { 
+
+	    	// console.log(d.date);
+	    	dateString = d.date.toString();
+	    	year = dateString.substring(dateString.length-4);
+	    	dateString = dateString.substring(0, dateString.length-4 )
+	    	day = dateString.substring(dateString.length-2);
+
+	    	day = (day.length > 1)? day : "0" + day
+
+	    	month = dateString.substring(0, dateString.length-2);
+	    	// console.log(year + "-0" + month + "-" + day);
+	    	return year + "-0" + month + "-" + day ; 
+
+	    })
+	    .rollup(function(d) { 
+	    	console.log(d[0].amount);
+	    	return parseInt(d[0].amount); 
+	    })
+	    .map(csv);
+
+	  console.log(data);
+
+	  rect.filter(function(d) { 
+	  	// console.log(d);
+	  	return d in data; })
+	      .attr("class", function(d) { 
+	      	// console.log(data[d]);
+	      	return "day " + color(data[d]) ; });
+
+	  
+	  var calendarTip = d3.tip().attr("class", "calendar-tip")
+		.html(function(d){
+
+			if (typeof d["amount"] === "undefined" ) {
+				return;
+			};
+
+			// console.log(d);
+			return '<div class="tip-content">' 
+				+ '<div>Donation: ' + d["amount"] + '</div>' 
+				+ '<div>' + "Date: " + d["date"] + '</div>'
+				+ '</div>';
+		
+		});
+
+	  svg.call(calendarTip);
+
+	  rect.on("mouseover", function(d){
+
+	  	// console.log(data[d]); 	// amount
+	  	// console.log(d);			// date
+
+		// calendarTip.show(data[d]);
+		calendarTip.show({
+			"date": d,
+			"amount": data[d]
+		});
+	  }).on("mouseout", function(d){
+		calendarTip.hide(data[d]);
+	  });
+
+
+
+	    // .select("title")
+	      // .text(function(d) { return d + ": " + percent(data[d]); });
+	});
+
+
+	function monthPath(t0) {
+	  var t1 = new Date(t0.getFullYear(), t0.getMonth() + 1, 0),
+	      d0 = t0.getDay(), w0 = d3.time.weekOfYear(t0),
+	      d1 = t1.getDay(), w1 = d3.time.weekOfYear(t1);
+	  return "M" + (w0 + 1) * cellSize + "," + d0 * cellSize
+	      + "H" + w0 * cellSize + "V" + 7 * cellSize
+	      + "H" + w1 * cellSize + "V" + (d1 + 1) * cellSize
+	      + "H" + (w1 + 1) * cellSize + "V" + 0
+	      + "H" + (w0 + 1) * cellSize + "Z";
+	}
+
+	d3.select(self.frameElement).style("height", "2910px");
+
+
+});
---
+++ js/map-bingo.js
@@ -0,0 +1,181 @@
+$(document).ready(function(){
+	
+
+	d3.json("../map/mi-oh-merge-data.topo.json", function(error, mi) {
+
+	    if (error) return console.error(error);
+
+	    d3.csv("../data/bingo-zip.csv", function(er, bingo){
+
+	    	if (error) return console.error(error);
+
+	    	var bingoDict = {}
+	    	console.log(bingo);
+	    	for (var i = 0; i < bingo.length; i++) {
+	    		bingoDict[bingo["ZCTA5CE10"]] = parseInt(bingo["amount"]);
+	    	};
+
+	        var width = 960,
+  			height = 560;
+
+
+		    // (40.755775, -73.982620) center for Manhattan island
+		    // this sets the scale that we can see all of the zip-code area with 
+		    var smallZoomScale = 5200;
+		    var smallZoomCenter = [-83.869629, 41.777457];
+		    // this sets the scale that we focus on the area where people play.
+		    var mediumZoomScale = 16000;
+		    var mediumZoomCenter = [-83.117065, 42.430299];
+
+		    var projection = d3.geo.mercator()
+		            .center(mediumZoomCenter)
+		            .scale(mediumZoomScale)
+		            .translate([(width) / 2, (height)/2]);
+
+		      var path = d3.geo.path()
+		      .projection(projection);
+
+		    /*svg.append("path")
+		        .datum( topojson.feature( nyc, nyc.objects.boroughs ))
+		        .attr("d", path);
+		    */
+
+		    var miMap = d3.select("body").select("#map-chart").append("svg")
+		    	.attr("width", width)
+		    	.attr("height", height);
+
+			miMap.selectAll(".zip")
+		        .data(topojson.feature(mi, mi.objects["mi-oh-rmpr"]).features)
+		      .enter().append("path")
+		        .attr("class", function(d) { 
+		          // console.log(d);
+		          return "zip"; })
+		        .attr("d", path)
+		        .attr("fill", function(d){
+
+		        	if ("donation" in d.properties){
+		        		// return "blue";
+		        		return "rgba(44,48,221,1)";
+		        	}
+		       		else{
+						// return "rgba(51,51,51,0.9)";
+						return "rgba(227,226,158,1)";
+		        	}
+		        })
+		        .style("stroke", "rgba(255,255,255,0.1)")
+		        .attr("stroke-width", 1);
+
+
+		    // place pin on the map
+		    // 1. (42.520795, -83.049812)
+		    // 2. (42.241616, -83.263993)
+
+		    // data saving where people play bingo
+		    var bingoPlaceData = [
+		    {
+		    	"name": "Green Acres Bingo Hall",
+		     	"address": "5653 E 13 Mile Rd Warren, MI",
+		     	"lat":42.520795,
+		     	"lng": -83.049812
+			}
+			/*,
+			{"name": "Democratic Club Bingo",
+		     "address": "23400 Wick Rd Taylor, Michigan 48180",
+		     "lat": 42.241616,
+		     "lng": -83.263993
+			}*/
+		    ];
+
+		    var bingoPlaces = miMap.selectAll(".bingo-place")
+		       .data(bingoPlaceData)
+		    	.enter().append("g")
+		    	.attr("class", "bingo-place")
+		    	.attr("transform", function(d){
+
+		    		console.log(d);
+
+			    	return "translate(" + projection([
+				              d.lng,
+				              d.lat
+				            ]) + ")";
+			    });
+
+			bingoPlaces.append("circle")
+		    	.attr("r", 8)
+		    	.attr("class", "bingo-circle")
+			    // .style("fill", "red");
+			    .style("fill", "rgba(190, 67, 47, 1)")
+
+			bingoPlaces.append("text")
+				.text(function(d){
+					return d.name;
+				})
+				.attr("transform", function(d){
+					return "translate(" + 18 + "," + 9 + ")";
+				})
+				.style("font-size", 15);
+
+			// these cities are shown to give audience an overview of where that is.
+			var citiesData = [
+			 {
+			 	"name": "Detroit",
+			 	"lat": 42.331427,
+			 	"lng": -83.045754
+			 }
+			]
+
+			var cities = miMap.selectAll(".city-g")
+			   .data(citiesData)
+				.enter().append("g")
+		    	.attr("class", "city-g")
+		    	.attr("transform", function(d){
+
+			    	return "translate(" + projection([
+				              d.lng,
+				              d.lat
+				            ]) + ")";
+			    });
+
+		    cities.append("circle")
+		    	.attr("r", 5)
+		    	.attr("class", "city-circle")
+			    .style("fill", "rgba(84, 82, 68, 1)");
+
+			cities.append("text")
+				.text(function(d){
+					return d.name;
+				})
+				.attr("transform", function(d){
+					return "translate(" + 18 + "," + 9 + ")";
+				})
+				.style("font-size", 15);
+
+
+			// append the thumbnail here
+			miMap.append("svg:image")
+				.attr("transform", function(d){
+					return "translate(" + 650 + "," + 360 + ")";
+				})
+				.attr("width", 322*0.8)
+				.attr("height", 233*0.8)
+				.attr("src", "img/map-thumbnail.png")
+				.attr("xlink:href", "img/map-thumbnail.png");
+
+			// append legend
+			miMap.append("rect")
+				.attr("width", 25)
+				.attr("height", 25)
+				.attr("transform", function(d){
+					return "translate(" + 720 + "," + 10 + ")";
+				})
+				.attr("fill", "red");
+
+
+
+
+	    });
+
+
+    });
+
+});
